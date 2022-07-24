c7eb73492ce74257f9b74b757078e498e4465d86
YuriiBachynskyi <bachinskii911@gmail.com>
2022-07-24 23:03:08 +0300

    Initial commit

---
+++ lesson1homework/main.js
@@ -0,0 +1,130 @@
+let str1;
+str1 ='hello';
+console.log(str1);
+alert(str1);
+document.write('hello');
+
+let str2;
+str2 ='owu';
+console.log(str2);
+alert(str2);
+document.write('owu');
+
+let str3;
+str3 ='com';
+console.log(str3);
+alert(str3);
+document.write('com');
+
+let str4;
+str4 ='ua';
+console.log(str4);
+alert(str4);
+document.write('ua');
+
+let a;
+a = 1;
+console.log(a);
+alert(a);
+document.write(a);
+
+let b;
+b = 10;
+console.log(b);
+alert(b);
+document.write(b);
+
+let c;
+c = -999;
+console.log(c);
+alert(c);
+document.write(c);
+
+let d;
+d = 123;
+console.log(d);
+alert(d);
+document.write(d);
+
+let e;
+e = 3.14;
+console.log(e);
+alert(e);
+document.write(e);
+
+let f;
+f = 2.7;
+console.log(f);
+alert(f);
+document.write(f);
+
+let g;
+g = 16;
+console.log(g);
+alert(g);
+document.write(g);
+
+let bool1;
+bool1 = true;
+console.log(bool1);
+alert(bool1);
+document.write(bool1);
+
+let bool2;
+bool2 = false;
+console.log(bool2);
+alert(bool2);
+document.write(bool2);
+
+let firstName;
+firstName ='Yurii';
+console.log(firstName);
+alert(firstName);
+document.write('Yurii');
+
+let middleName;
+middleName ='Romanovych';
+console.log(middleName);
+alert(middleName);
+document.write('Romanovych');
+
+let lastName;
+lastName ='Bachynskyi';
+console.log(lastName);
+alert(lastName);
+document.write('Bachynskyi');
+
+let person;
+person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+let a1;
+a1 = 100;
+console.log(typeof a1);
+
+let b1;
+b1 = 100;
+console.log(typeof b1);
+
+let c1;
+c1 = true;
+console.log(typeof c1);
+
+let inputdatafirstName;
+inputdatafirstName = prompt('firstName');
+console.log(inputdatafirstName);
+
+let inputdatamiddleName;
+inputdatamiddleName = prompt('middleName');
+console.log(inputdatamiddleName);
+
+let years;
+years = prompt('years');
+console.log(years);
+
+
+
+
+
+
+
